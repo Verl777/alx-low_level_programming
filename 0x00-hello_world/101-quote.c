744b458a84fb40fccd3cb83c323f7b06778883af
Verl777 <morahvalerianne@gmail.com>
2023-02-16 11:25:10 -0800

    C program without using printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
-#include <stdio.h> 
+#include <unistd.h>
+
 /**
- * main - prints exactly and that piece of art is useful
- * Return: 1 success
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *
+ * Return:  1 success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",100);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      80);
 
 	return (1);
 }
+
