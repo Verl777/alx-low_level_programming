97330bc90d237bc42c5175dcf529fa2f915aa9f6
Verl777 <morahvalerianne@gmail.com>
2023-02-16 10:14:25 -0800

    Using the function puts to print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main the entry point of a program
- * Return 0 should be a success
+ * main - prints the output
+ * Return: 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
