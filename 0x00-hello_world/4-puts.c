ed5cfa7008bd7d9960b79e2fee9d98b50fc5ba5f
Verl777 <morahvalerianne@gmail.com>
2023-02-16 10:02:41 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
-#!/bin/bash
-int main()
+#include <stdio.h>
+/*mai the entry point of a program
+ * Return 0 should be a success
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+	return (0);
 }
