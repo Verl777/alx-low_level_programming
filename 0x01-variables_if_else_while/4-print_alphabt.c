0cc427f2f387c040a674024cb03492452cf323b5
Verl777 <morahvalerianne@gmail.com>
2023-02-17 11:04:23 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lowercase except q and e
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	}
+	putchat('\n');
+	return (0);
