39f5555f0b9a41a91850e2e4e51a5fa2bedc5832
Verl777 <morahvalerianne@gmail.com>
2023-02-21 07:29:12 -0800

    prints letters in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,14 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "main.h"
 /**
- * main - entry point
+ * main - prints letters in lowercase
  * Return: always a success
  */
+void print_alphabet(void)
+{
+	char c;
 
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
