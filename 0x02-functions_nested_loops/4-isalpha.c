cb07ae47762b0eeee3f1c5f937a2397aa3b528f9
Verl777 <morahvalerianne@gmail.com>
2023-02-21 10:25:32 -0800

    checks for alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks for the alphabetic character
+ * @c: the character to be checked
+ * Return: 1 for true  0 for else
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
