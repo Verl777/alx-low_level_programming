b527156b473530655446b88ae4b7a8fe2719d487
Verl777 <morahvalerianne@gmail.com>
2023-02-23 10:04:41 -0800

    multply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplie 2 ints
+ * @a: int to multiply
+ * @b: int to multiply
+ * Return: sucess
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
