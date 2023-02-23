20839b0aff47dd5d3cb112260452abd025c69a2e
Verl777 <morahvalerianne@gmail.com>
2023-02-23 04:02:39 -0800

    Debug for + or -

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,6 +3,7 @@
  * main - Prints whether a number stored is positive or negatine
  * Return: success
  */
+
 int main(void)
 {
 	int i;
@@ -10,5 +11,6 @@ int main(void)
 	i = 0;
 
 	positive_or_negative(i);
+
 	return (0);
 }
