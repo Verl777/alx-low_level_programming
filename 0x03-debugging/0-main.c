dc128e7759f24ab610fecb9d7ec5380e9bb581b9
Verl777 <morahvalerianne@gmail.com>
2023-02-23 00:45:32 -0800

    test for a positive or negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints whether a number stored is positive or negatine
+ * Return: success
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
