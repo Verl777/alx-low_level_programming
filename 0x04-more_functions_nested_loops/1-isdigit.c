6627b5fde00a442afa94e24bd25dbddf4ce4389a
Verl777 <morahvalerianne@gmail.com>
2023-02-23 08:12:31 -0800

    checks for digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,13 +5,13 @@
  * @c: integer to be checked
  * Return: always 0
  */
+
 int _isdigit(int c);
 {
-	if (c >= 0  && c <= 9)
-	{
+	if (c >= '0'  && c <= '9')
+	
 		return (1);
-	}
-
-	return (0);
+	else
+		return (0);
 }
 
