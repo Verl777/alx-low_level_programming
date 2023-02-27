d78edaae7ade5d55555b8d6aa77391fdc626f217
Verl777 <morahvalerianne@gmail.com>
2023-02-27 11:20:48 -0800

    swaps ints

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "nain.h"
+
+/**
+ * swap_int - swaps value of two ints
+ * @a: first int
+ * @b: second int
+ * Return: 0 Always sucess
+ */
+
+void swap_int(int *a, int *b)
+{
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
+}
