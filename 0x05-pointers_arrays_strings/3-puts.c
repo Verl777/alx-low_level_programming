4c84c0b19e5537ed93e199349641081d7dd7f6da
Verl777 <morahvalerianne@gmail.com>
2023-02-27 12:44:42 -0800

    prints string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string
+ * Return: success
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
