332f72175df5a9f18ee51c97cf612d38b5775420
Verl777 <morahvalerianne@gmail.com>
2023-03-07 07:33:56 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,12 +8,12 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int c = 0;
-	int b = 0;
+	int c;
+	int b;
 
-	for (; c < 8; c++)
+	for (c = 0; c < 8; c++)
 	{
-		for (; b < 8; b++)
+		for (b = 0; b < 8; b++)
 			_putchar(a[c][b]);
 		_putchar('\n');
 	}
